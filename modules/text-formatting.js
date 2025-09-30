83d3438fbe2602b332cea1e51ace3863e887d2b9
sumer1hp <summer1hp@mail.ru>
2025-10-01 01:18:36 +0300

    Update text-formatting.js

--- modules/text-formatting.js
+++ modules/text-formatting.js
@@ -21,7 +21,7 @@ class TextFormatting {
     applyFormat(formatType, color = null) {
         const activeElement = document.activeElement;
         if (!activeElement || !activeElement.classList.contains('text-input')) {
-            showNotification('Выделите текстовое поле для форматирования', 'warning');
+            if (window.showAlert) window.showAlert('Выделите текстовое поле для форматирования', 'warning');
             return;
         }
 
@@ -30,7 +30,7 @@ class TextFormatting {
         const text = activeElement.value;
         
         if (start === end) {
-            showNotification('Выделите текст для форматирования', 'warning');
+            if (window.showAlert) window.showAlert('Выделите текст для форматирования', 'warning');
             return;
         }
 
@@ -39,11 +39,11 @@ class TextFormatting {
 
         if (color) {
             // Цветное форматирование для ASS
-            formattedText = `{\\c${this.colors[color]}}${selectedText}{\\c}`;
+            formattedText = '{\\c' + this.colors[color] + '}' + selectedText + '{\\c}';
         } else if (this.formats[formatType]) {
             // Обычное форматирование
             const format = this.formats[formatType];
-            formattedText = `{${format.ass}}${selectedText}{${format.end.ass}}`;
+            formattedText = '{' + format.ass + '}' + selectedText + '{' + format.end.ass + '}';
         }
 
         const newText = text.substring(0, start) + formattedText + text.substring(end);
@@ -53,7 +53,9 @@ class TextFormatting {
         const event = new Event('input', { bubbles: true });
         activeElement.dispatchEvent(event);
 
-        showNotification(`Форматирование применено: ${formatType}${color ? ' ' + color : ''}`, 'success');
+        if (window.showAlert) {
+            window.showAlert('Форматирование применено: ' + formatType + (color ? ' ' + color : ''), 'success');
+        }
     }
 
     // Очистка всего форматирования
@@ -96,51 +98,58 @@ function formatText(formatType, color = null) {
 }
 
 function showFormattingModal() {
-    const modalHTML = `
-        <div class="mb-3">
-            <h6>Форматирование текста</h6>
-            <div class="btn-group w-100 mb-2">
-                <button class="btn btn-outline-dark" onclick="formatText('bold')">
-                    <i class="bi bi-type-bold"></i> Жирный
-                </button>
-                <button class="btn btn-outline-dark" onclick="formatText('italic')">
-                    <i class="bi bi-type-italic"></i> Курсив
-                </button>
-                <button class="btn btn-outline-dark" onclick="formatText('underline')">
-                    <i class="bi bi-type-underline"></i> Подчеркивание
-                </button>
-            </div>
-            
-            <h6 class="mt-3">Цвета</h6>
-            <div class="btn-group w-100">
-                <button class="btn btn-outline-danger" onclick="formatText('color', 'red')">Красный</button>
-                <button class="btn btn-outline-success" onclick="formatText('color', 'green')">Зеленый</button>
-                <button class="btn btn-outline-primary" onclick="formatText('color', 'blue')">Синий</button>
-                <button class="btn btn-outline-warning" onclick="formatText('color', 'yellow')">Желтый</button>
-            </div>
-            
-            <div class="mt-3">
-                <button class="btn btn-outline-secondary w-100" onclick="clearAllFormatting()">
-                    <i class="bi bi-eraser"></i> Очистить все форматирование
-                </button>
-            </div>
-        </div>
-    `;
+    const modalHTML = '<div class="mb-3">' +
+        '<h6>Форматирование текста</h6>' +
+        '<div class="btn-group w-100 mb-2">' +
+            '<button class="btn btn-outline-dark" onclick="formatText(\'bold\')">' +
+                '<i class="bi bi-type-bold"></i> Жирный' +
+            '</button>' +
+            '<button class="btn btn-outline-dark" onclick="formatText(\'italic\')">' +
+                '<i class="bi bi-type-italic"></i> Курсив' +
+            '</button>' +
+            '<button class="btn btn-outline-dark" onclick="formatText(\'underline\')">' +
+                '<i class="bi bi-type-underline"></i> Подчеркивание' +
+            '</button>' +
+        '</div>' +
+        '<h6 class="mt-3">Цвета</h6>' +
+        '<div class="btn-group w-100">' +
+            '<button class="btn btn-outline-danger" onclick="formatText(\'color\', \'red\')">Красный</button>' +
+            '<button class="btn btn-outline-success" onclick="formatText(\'color\', \'green\')">Зеленый</button>' +
+            '<button class="btn btn-outline-primary" onclick="formatText(\'color\', \'blue\')">Синий</button>' +
+            '<button class="btn btn-outline-warning" onclick="formatText(\'color\', \'yellow\')">Желтый</button>' +
+        '</div>' +
+        '<div class="mt-3">' +
+            '<button class="btn btn-outline-secondary w-100" onclick="clearAllFormatting()">' +
+                '<i class="bi bi-eraser"></i> Очистить все форматирование' +
+            '</button>' +
+        '</div>' +
+    '</div>';
 
-    const modal = new bootstrap.Modal(document.createElement('div'));
-    modal._element.innerHTML = `
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Форматирование текста</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">${modalHTML}</div>
-            </div>
-        </div>
-    `;
+    // Создаем модальное окно
+    const modalId = 'formattingModal';
+    let modalElement = document.getElementById(modalId);
     
-    document.body.appendChild(modal._element);
+    if (!modalElement) {
+        modalElement = document.createElement('div');
+        modalElement.id = modalId;
+        modalElement.className = 'modal fade';
+        modalElement.tabIndex = -1;
+        modalElement.innerHTML = 
+            '<div class="modal-dialog">' +
+                '<div class="modal-content">' +
+                    '<div class="modal-header">' +
+                        '<h5 class="modal-title">Форматирование текста</h5>' +
+                        '<button type="button" class="btn-close" data-bs-dismiss="modal"></button>' +
+                    '</div>' +
+                    '<div class="modal-body">' + modalHTML + '</div>' +
+                '</div>' +
+            '</div>';
+        document.body.appendChild(modalElement);
+    } else {
+        modalElement.querySelector('.modal-body').innerHTML = modalHTML;
+    }
+    
+    const modal = new bootstrap.Modal(modalElement);
     modal.show();
 }
 
@@ -154,5 +163,5 @@ function clearAllFormatting() {
     
     if (window.renderTable) renderTable();
     window.dispatchEvent(new CustomEvent('subtitlesChanged'));
-    showNotification('Все форматирование очищено', 'success');
+    if (window.showAlert) window.showAlert('Все форматирование очищено', 'success');
 }
